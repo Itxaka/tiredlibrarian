ce85b6b22a90fa40a1b27b0c01294aa99b39fcfa
Itxaka <igarcia@suse.com>
2019-06-20 18:40:47 +0200

    fix version

--- main/version.py
+++ main/version.py
@@ -7,7 +7,7 @@ FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 def get_version_from_git():
     print("getting version")
     try:
-        return subprocess.check_output(['git', 'describe', '--tags'],
+        return subprocess.check_output(['git', 'describe', '--tags', '--always'],
                                        cwd=FILE_DIR).decode('utf-8').strip()
     except:
         return '?'
